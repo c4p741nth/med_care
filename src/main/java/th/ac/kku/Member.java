df6f22992da41afc58885fb8b2084a60e5627f26
C4P741N <prompiriya.cpt@gmail.com>
2023-10-06 21:20:14 +0700

    member_database

--- src/main/java/th/ac/kku/Member.java
+++ src/main/java/th/ac/kku/Member.java
@@ -1,7 +1,22 @@
 package th.ac.kku;
 
+import java.sql.Date;
+
 public class Member {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+    private int id;
+    private String firstname;
+    private String lastname;
+    private int gender;
+    private Date birthDate;
+    private String address;
+    private String mobile;
+    private String email;
+    private String password;
+
+    // Medical information
+    private String allergic;
+    private String blood_group;
+    private double weight;
+    private double height;
+    private String chronic_disease;
 }
--- src/main/java/th/ac/kku/MemberDAO.java
+++ src/main/java/th/ac/kku/MemberDAO.java
@@ -1,5 +1,7 @@
 package th.ac.kku;
 
 public class MemberDAO {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
