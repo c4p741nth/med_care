c741072945a830c20641af0e81d09401bdc942b9
C4P741N <prompiriya.cpt@gmail.com>
2023-10-06 21:08:22 +0700

    fix main method

--- src/main/java/th/ac/kku/Member.java
+++ src/main/java/th/ac/kku/Member.java
@@ -1,7 +1,7 @@
 package th.ac.kku;
 
 public class Member {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
