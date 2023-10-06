d750083706e31069668eac8abc03b5e0edecbb96
C4P741N <prompiriya.cpt@gmail.com>
2023-10-06 21:59:39 +0700

    migrate_members

--- src/main/java/th/ac/kku/Migrate.java
+++ src/main/java/th/ac/kku/Migrate.java
@@ -17,24 +17,24 @@ public class Migrate {
             connection = DriverManager.getConnection(jdbcUrl, username, password);
             Statement statement = connection.createStatement();
 
-            String createTableSQL = "CREATE TABLE members ("
-                    + "id SERIAL PRIMARY KEY,"
-                    + "citizen_id VARCHAR(13),"
-                    + "firstname VARCHAR(50),"
-                    + "lastname VARCHAR(50),"
-                    + "gender INT(1),"
-                    + "birthDate DATE,"
-                    + "address TEXT,"
-                    + "mobile VARCHAR(10),"
-                    + "email VARCHAR(255),"
-                    + "password VARCHAR(255),"
-                    + "allergic TEXT,"
-                    + "blood_group VARCHAR(2),"
-                    + "weight DOUBLE PRECISION,"
-                    + "height DOUBLE PRECISION,"
-                    + "chronic_disease TEXT,"
-                    + "accessLevel INT(1)"
-                    + ")";
+            String createTableSQL = "CREATE TABLE members (" +
+                    "id SERIAL PRIMARY KEY," +
+                    "citizen_id VARCHAR(20) NOT NULL," +
+                    "firstname VARCHAR(50) NOT NULL," +
+                    "lastname VARCHAR(50) NOT NULL," +
+                    "gender INT NOT NULL," +
+                    "birth_date DATE NOT NULL," +
+                    "address VARCHAR(255)," +
+                    "mobile VARCHAR(15)," +
+                    "email VARCHAR(100)," +
+                    "password VARCHAR(100)," +
+                    "allergic VARCHAR(255)," +
+                    "blood_group VARCHAR(10)," +
+                    "weight DOUBLE PRECISION," +
+                    "height DOUBLE PRECISION," +
+                    "chronic_disease VARCHAR(255)," +
+                    "accessLevel INT" +
+                    ")";
 
             statement.executeUpdate(createTableSQL);
 
